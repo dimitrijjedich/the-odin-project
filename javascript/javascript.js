3a3a1e53455f1fc0033ddde7a61a36a3461b6225
Dimitrij Jedich <140393501+dimitrijjedich@users.noreply.github.com>
2023-11-18 10:29:43 +0100

    Fundamentals Part 1 (#30)
    * add inital setup
    
    * add external javascript file and usage of variables
    
    * add example for copy of variable content to another one
    
    * add missing equal sign
    
    * add simple assignment

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!")
+
+let message;
+message = "Hello";
+
+alert(message);
+
+let user = "John", age = 25, mess = "Hello, John";
+
+let x = "Message";
+let y = x;
+
+let a = 10;
+console.log(a);
+let b = a * 9;
+console.log(b);
