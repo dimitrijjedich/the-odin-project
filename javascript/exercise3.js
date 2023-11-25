fb578b0a629b5f6d8929ca8410233faf3c5c83ad
dimitrijjedich <dimitrij@startup-werk.de>
2023-11-25 21:21:20 +0100

    change a to correct calculation

--- javascript/exercise3.js
+++ javascript/exercise3.js
@@ -5,7 +5,7 @@
  *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
  */
 
-const a = "one plus eight"
+const a = 1+8;
 const b = "22 times three"
 const c = "the *remainder* of 5/4"
 const d = "the variable 'a' minus 17"
