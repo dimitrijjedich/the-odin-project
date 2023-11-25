31def792ec10ff3092c5d2045ce898cbf49940ea
Dimitrij Jedich <dimitrij@startup-werk.de>
2023-11-25 23:38:21 +0100

    replace greetings contatination with new variables

--- javascript/exercise4.js
+++ javascript/exercise4.js
@@ -82,4 +82,4 @@ let firstName = "Carlos";
 let lastName = "Stevenson";
 let fullName = firstName + " " + lastName;
 let age = thisYear - birthYear;
-let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
