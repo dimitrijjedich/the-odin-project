55f7cf709e1bf1558858c3ca16aed812539d7070
dimitrijjedich <dimitrij@startup-werk.de>
2023-12-09 13:46:52 +0100

    add initial solution for sumAll

--- javascript/exercise/05_sumAll/sumAll.js
+++ javascript/exercise/05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let result = 0;
+  for(let i=start; i<=end; i++) {
+    result = result + i;
+  }
+  return result;
 };
 
 // Do not edit below this line
