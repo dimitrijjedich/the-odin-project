8bfedb7f8422dc2e595d2dceea58e07d81ec2314
dimitrijjedich <dimitrij@startup-werk.de>
2023-12-09 13:12:14 +0100

    add initial solution for removeFromArray

--- javascript/exercise/04_removeFromArray/removeFromArray.js
+++ javascript/exercise/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, element) {
+  let result = [];
+  for(let i=0; i<array.length; i++) {
+    if (array[i] != element) {
+      result.push(array[i]);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
