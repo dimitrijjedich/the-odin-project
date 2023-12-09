a2391d2556be4b91bdf751751ebad5212a25684b
dimitrijjedich <dimitrij@startup-werk.de>
2023-12-09 13:50:37 +0100

    add check if start is greater then and with switch if true

--- javascript/exercise/05_sumAll/sumAll.js
+++ javascript/exercise/05_sumAll/sumAll.js
@@ -1,4 +1,9 @@
 const sumAll = function(start, end) {
+  if ( start > end ) {
+    start = start + end;
+    end = start - end;
+    start = start - end;
+  }
   let result = 0;
   for(let i=start; i<=end; i++) {
     result = result + i;
