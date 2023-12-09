12811a24f84a2c0ce96c50c69142e4810174ec38
dimitrijjedich <dimitrij@startup-werk.de>
2023-12-09 14:18:38 +0100

    add function round

--- javascript/exercise/07_tempConversion/tempConversion.js
+++ javascript/exercise/07_tempConversion/tempConversion.js
@@ -6,6 +6,11 @@ const convertToFahrenheit = function(temp) {
   return (temp*9/5)+32;
 };
 
+function round(number) {
+  let result = number *10;
+  result = Math.round(result);
+  return result / 10;
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
