1718aace1039b329681caa5dad46ceaeba4193b3
dimitrijjedich <dimitrij@startup-werk.de>
2023-12-09 13:39:26 +0100

    add additional parameters to function

--- javascript/exercise/04_removeFromArray/removeFromArray.js
+++ javascript/exercise/04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
-const removeFromArray = function(array, element) {
+const removeFromArray = function(array, element1, element2=null, element3=null, element4=null) {
   let result = [];
   for(let i=0; i<array.length; i++) {
-    if (array[i] != element) {
+    if (!(array[i] === element1) && !(array[i] === element2) && !(array[i] === element3) && !(array[i] === element4)) {
       result.push(array[i]);
     }
   }
