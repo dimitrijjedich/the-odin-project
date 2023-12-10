9c519c8bdf4a5838174807eb0723c3680cca0b39
Dimitrij Jedich <dimitrij@startup-werk.de>
2023-12-10 18:27:36 +0100

    refactor to initial solution

--- javascript/exercise/04_removeFromArray/removeFromArray.js
+++ javascript/exercise/04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
-const removeFromArray = function(array, element1, element2=null, element3=null, element4=null) {
+const removeFromArray = function(array, ...elements) {
   let result = [];
   for(let i=0; i<array.length; i++) {
-    if (!(array[i] === element1) && !(array[i] === element2) && !(array[i] === element3) && !(array[i] === element4)) {
-      result.push(array[i]);
+    for (let j=0; j<elements.length; j++) {
+      console.log("arr"+array[i]);
+      console.log("elements"+elements[j]);
+      if (!(array[i]===elements[j])) {
+        result.push(array[i]);
+      }
     }
   }
   return result;
