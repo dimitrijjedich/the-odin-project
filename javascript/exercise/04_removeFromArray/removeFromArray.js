b4e2aff00464a51305c2dbc70fcc8316d4df109b
Dimitrij Jedich <dimitrij@startup-werk.de>
2023-12-10 18:30:35 +0100

    introduce check variable to add elements to results array

--- javascript/exercise/04_removeFromArray/removeFromArray.js
+++ javascript/exercise/04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
 const removeFromArray = function(array, ...elements) {
   let result = [];
   for(let i=0; i<array.length; i++) {
+    let check = true;
     for (let j=0; j<elements.length; j++) {
-      if (!(array[i]===elements[j])) {
-        result.push(array[i]);
+      if (array[i]===elements[j]) {
+        check = false;
       }
+    }
+    if (check) {
+      result.push(array[i]);
     }
   }
   return result;
